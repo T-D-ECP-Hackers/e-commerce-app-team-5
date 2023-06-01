1a4b49931b52755b5757f5301511ed97be547f21
heidir2 <heidi.rae@global.com>
2023-06-01 15:56:56 +0100

    more changes

--- frontend/src/components/pages/RootPage.tsx
+++ frontend/src/components/pages/RootPage.tsx
@@ -4,6 +4,9 @@ import { useNavigate } from "react-router-dom";
 import { fetchBasket } from "../../api/fetchBasket";
 import BasketContext from "../../context/BasketContext";
 
+
+
+
 function RootPage() {
   const basket = useContext(BasketContext);
   const navigate = useNavigate();
@@ -12,16 +15,26 @@ function RootPage() {
     fetchBasket(basket.setCurrentBasket);
   }, []);
 
-  return (
-    <div className="app-body">
-      <div
-        onClick={() => goToProductsPage(navigate)}
-        className="products-block"
-      >
-        <label>View all our products!</label>
-      </div>
-    </div>
-  );
+
+    useEffect(() => {
+        fetchBasket(basket.setCurrentBasket);
+    }, []);
+
+
+
+    return (
+        <div className="app-body">
+            <div onClick={() => goToProductsPage(navigate)} className="products-block">
+                <label>
+                    <span className='merch-header'>The best merch in town. As long as you </span> 
+                    <span className='hack-header'>hack </span>
+                    <span className='me-header'>me... </span>
+                </label>
+            </div>
+        </div>
+        
+    );
 }
 
 export default RootPage;
+
